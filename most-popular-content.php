b3d7b6933745abc898bd0c7f90cb39a2f9994c78
Rob Rogers <robrogers@me.com>
2012-11-28 21:19:43 -0800

    add licensing

--- most-popular-content.php
+++ most-popular-content.php
@@ -1,10 +1,27 @@
 <?php
 /*
 Plugin Name: Most Popular Content
-Plugin URI: http://aeideas.stratecomm.net
+Plugin URI: http://www.stratecomm.com
 Description: Most Popular content tabbed widget to show: most viewed, most commented, most shared posts in 3 tabs.
-Author: vietdt
+Author: vietdt,rrogers
 Version: 1.0
+License: GPLv2 or later
+*/
+
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
 global $sharing_stats_db_version;
